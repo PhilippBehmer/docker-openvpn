2fed77c8dd79b0ecaeb8c1de74ac9f9560fe6da5
Philipp Behmer <kontakt@philippbehmer.de>
2021-02-04 17:40:12 +0100

    Add configcheck to start script

--- openvpn_start.sh
+++ openvpn_start.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+SERVERFILE=/etc/openvpn/server.conf
+CLIENTFILE=/etc/openvpn/client.conf
+
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
-openvpn --config /etc/openvpn/server.conf
+
+if [ -f "$SERVERFILE" ]; then
+    echo "Starting OpenVPN server..."
+    openvpn --config /etc/openvpn/server.conf
+elif [ -f "$CLIENTFILE" ]; then
+    echo "Starting OpenVPN client..."
+    openvpn --config /etc/openvpn/client.conf
+else
+    echo "No config file found!"
+    exit 1
+fi
+
